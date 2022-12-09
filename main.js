a0503c60dc819dc1300045953df02e825f3ff0ad
Kevin-AC <kevin.argumedo@gmail.com>
2022-12-08 22:27:13 -0500

    mejoras css

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let shareBtn = document.querySelector('.main-container__perfil--icon');
 let sahareContainer = document.querySelector('.main-container__share')
-let perfilContainer = document.querySelector('.main-container__perfil')
+let profileContainer = document.querySelector('.main-container__perfil')
 shareBtn.addEventListener('click',event=>{
-    let presbtn=event.target.innerText;
-    if(presbtn==''){
-        sahareContainer.style.display='flex'
-        perfilContainer.style.display='none'
-    }
+    let presbtn=event.target.onclick;
+    console.log(presbtn)
+     if(presbtn==null){
+         sahareContainer.style.display='flex'
+         profileContainer.style.display='none'
+     }
 })
